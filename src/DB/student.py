46167ba41786a1373ae7ac6f2b9d5e7ec41da72f
SleepyWoodpecker <shawnweichew@gmail.com>
2025-02-13 07:29:03 -0800

    updated student table to reflect schema changes
    relevant schema update can be found in 8f12601a5b6863b98a992e209311ecc21133710a

--- src/DB/student.py
+++ src/DB/student.py
@@ -5,6 +5,7 @@ from sqlmodel import Field
 class Student(Base, table=True):
     id: int = Field(primary_key=True)
     name: str = Field(nullable=False)
+    teacher_id: int = Field(foreign_key="teacher.id")
 
     def __repr__(self) -> str:
         return f"Student(id={self.id!r}, name={self.name!r})"
