d27a4c2231efdb269b043caf4542b39fa40daeb2
SleepyWoodpecker <shawnweichew@gmail.com>
2025-02-15 11:51:08 -0800

    updated imports

--- src/DB/__init__.py
+++ src/DB/__init__.py
@@ -1,6 +1,6 @@
 from DB.student import Student, StudentDB
-import DB.teacher
-import DB.course
+from DB.teacher import Teacher
+from DB.course import Course_Record
 from DB.DB import init_db
 
 from DB.db_exceptions import DBAPIError, DBConnectionError, DBRecordNotFoundError
